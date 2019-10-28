b239e696a25fe163aa9d7d1c95d2de8c05306666
a7alammar <abdulrahman.alammar7@gmail.com>
2019-10-29 01:57:48 +0300

    second commit

--- sw.js
+++ sw.js
@@ -5,13 +5,12 @@ let staticCacheName = 'mws-static-v1';
 self.addEventListener('install', function (event){
 event.waitUntil(caches.open(staticCacheName).then(function(cache){
 return cache.addAll(['./',
-'js/main.js', 'js/restaurant_info.js', 'js/dbhelper.js', 'js/registeration.js',
+'js/main.js', 'js/restaurant_info.js', 'js/dbhelper.js', 
 'css/styles.css',
 'img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg', 'img/5.jpg', 'img/6.jpg', 'img/7.jpg', 'img/8.jpg', 'img/9.jpg', 'img/10.jpg',
 'data/restaurants.json',
 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
-'//normalize-css.googlecode.com/svn/trunk/normalize.css'
       ]);
   }));
 });
